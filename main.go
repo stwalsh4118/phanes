37810d3834a1e87a9e312fee630c47ed461afb75
stwalsh4118 <seanwalsh4118@yahoo.com>
2025-12-25 11:27:50 -0600

    chore(nginx): register module and mark task 5-1 as done
    - Register NginxModule in registerAllModules()
    - Update task 5-1 status to Done
    - Add status history entries for task completion
    
    Task: 5-1

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stwalsh4118/phanes/internal/modules/baseline"
 	"github.com/stwalsh4118/phanes/internal/modules/docker"
 	"github.com/stwalsh4118/phanes/internal/modules/monitoring"
+	"github.com/stwalsh4118/phanes/internal/modules/nginx"
 	"github.com/stwalsh4118/phanes/internal/modules/security"
 	"github.com/stwalsh4118/phanes/internal/modules/swap"
 	"github.com/stwalsh4118/phanes/internal/modules/updates"
@@ -297,6 +298,7 @@ func registerAllModules() *runner.Runner {
 	r.RegisterModule(&updates.UpdatesModule{})
 	r.RegisterModule(&docker.DockerModule{})
 	r.RegisterModule(&monitoring.MonitoringModule{})
+	r.RegisterModule(&nginx.NginxModule{})
 
 	return r
 }
