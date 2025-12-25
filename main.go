f0c809882719f8995929e23a19440b3566d7afd2
stwalsh4118 <seanwalsh4118@yahoo.com>
2025-12-25 13:08:32 -0600

    feat(main): register RedisModule in module registry
    Register RedisModule in registerAllModules() function to enable Redis installation via profiles and --modules flag.

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stwalsh4118/phanes/internal/modules/monitoring"
 	"github.com/stwalsh4118/phanes/internal/modules/nginx"
 	"github.com/stwalsh4118/phanes/internal/modules/postgres"
+	"github.com/stwalsh4118/phanes/internal/modules/redis"
 	"github.com/stwalsh4118/phanes/internal/modules/security"
 	"github.com/stwalsh4118/phanes/internal/modules/swap"
 	"github.com/stwalsh4118/phanes/internal/modules/updates"
@@ -303,6 +304,7 @@ func registerAllModules() *runner.Runner {
 	r.RegisterModule(&nginx.NginxModule{})
 	r.RegisterModule(&caddy.CaddyModule{})
 	r.RegisterModule(&postgres.PostgresModule{})
+	r.RegisterModule(&redis.RedisModule{})
 
 	return r
 }
